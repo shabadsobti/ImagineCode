d8534cb34d127eda4160d26f4b35873f328123fc
LAPTOP-7ERJTKE5\Roman <roman.sharykin@gmail.com>
2018-06-13 13:28:51 -0400

    Lesson screen

--- app/src/main/java/com/imaginecode/imaginecode/IntroModActivity.java
+++ app/src/main/java/com/imaginecode/imaginecode/IntroModActivity.java
@@ -6,13 +6,23 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.GridView;
+
+import java.util.ArrayList;
 
 public class IntroModActivity extends AppCompatActivity {
+
+    ArrayList<LessonClass> lessons;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_intro);
+
+        GridView gridView = (GridView)findViewById(R.id.gridview);
+        lessons = LessonClass.generateInitialLessons();
+        LessonAdapter lessonsAdapter = new LessonAdapter(this, lessons);
+        gridView.setAdapter(lessonsAdapter);
 
     }
 
---
+++ app/src/main/java/com/imaginecode/imaginecode/LessonAdapter.java
@@ -0,0 +1,71 @@
+package com.imaginecode.imaginecode;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+public class LessonAdapter extends BaseAdapter {
+
+    private final Context mContext;
+    private final ArrayList<LessonClass>  lessons;
+
+    public LessonAdapter(Context context, ArrayList<LessonClass> lessons) {
+        this.mContext = context;
+        this.lessons = lessons;
+    }
+
+    @Override
+    public int getCount() {
+        return lessons.size();
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    // 4
+    @Override
+    public Object getItem(int position) {
+        return null;
+    }
+
+    // 5
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        final LessonClass lesson = lessons.get(position);
+
+        if (convertView == null) {
+            final LayoutInflater layoutInflater = LayoutInflater.from(mContext);
+            convertView = layoutInflater.inflate(R.layout.cell_lesson, null);
+        }
+
+        final TextView lesson_num = (TextView)convertView.findViewById(R.id.lesson_num);
+        final ImageView star1 = (ImageView)convertView.findViewById(R.id.imageview_1);
+        final ImageView star2 = (ImageView)convertView.findViewById(R.id.imageview_2);
+        final ImageView star3 = (ImageView)convertView.findViewById(R.id.imageview_3);
+
+        lesson_num.setText(lesson.getNumber());
+
+        if(lesson.getStars() == 1){
+            star1.setImageResource(R.drawable.star_lvlcomp);
+        }
+        else if(lesson.getStars() == 2){
+            star1.setImageResource(R.drawable.star_lvlcomp);
+            star2.setImageResource(R.drawable.star_lvlcomp);
+        }
+        else if(lesson.getStars() == 3){
+            star1.setImageResource(R.drawable.star_lvlcomp);
+            star2.setImageResource(R.drawable.star_lvlcomp);
+            star3.setImageResource(R.drawable.star_lvlcomp);
+        }
+
+        return convertView;
+    }
+}
