4e56ed2e32f31dd3f5a6e0e858ca3fe67cae264b
LAPTOP-7ERJTKE5\Roman <roman.sharykin@gmail.com>
2018-06-13 13:37:18 -0400

    Back button hooked up

--- app/src/main/java/com/imaginecode/imaginecode/IntroModActivity.java
+++ app/src/main/java/com/imaginecode/imaginecode/IntroModActivity.java
@@ -1,11 +1,13 @@
 package com.imaginecode.imaginecode;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
 import android.widget.GridView;
 
 import java.util.ArrayList;
@@ -13,6 +15,7 @@ import java.util.ArrayList;
 public class IntroModActivity extends AppCompatActivity {
 
     ArrayList<LessonClass> lessons;
+    Button back;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +27,15 @@ public class IntroModActivity extends AppCompatActivity {
         LessonAdapter lessonsAdapter = new LessonAdapter(this, lessons);
         gridView.setAdapter(lessonsAdapter);
 
+        back = (Button)findViewById(R.id.back);
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent myIntent = new Intent(IntroModActivity.this,
+                        ModulesActivity.class);
+                startActivity(myIntent);
+            }
+        });
     }
 
 }
