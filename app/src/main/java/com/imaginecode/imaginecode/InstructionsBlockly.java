0f9a04fbfd3a98d839594ff56eae21b914f01e92
Shabad Sobti <shabad.sobti@gmail.com>
2018-08-14 16:38:35 -0400

    Alot of changed :)

---
+++ app/src/main/java/com/imaginecode/imaginecode/InstructionsBlockly.java
@@ -0,0 +1,13 @@
+package com.imaginecode.imaginecode;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class instructions_blockly extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_instructions_blockly);
+    }
+}
